75422efdddf4e9c5f04a197344fe8fda53666b08
Tommy <afteryou725@gmail.com>
2021-07-08 15:44:08 +0200

    Xstream XML save

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-        Settings set = new Settings();
-        new Clicker();
+        try {
+            Settings set = new Settings();
+            TurboSaver saver = new TurboSaver();
+            saver.saveToXML(set);
+            System.out.println(saver.readFromXML().isHoldMode());
+            new Clicker();
+        }catch(IOException ex){
+            System.out.println("Error: "+ex);
+        }
     }
 }
 
--- src/TurboSaver.java
+++ src/TurboSaver.java
@@ -1,12 +1,34 @@
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.security.*;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collection;
 
 public class TurboSaver {
     XStream xstream;
     public TurboSaver() {
         xstream = new XStream();
         xstream.alias("settings",Settings.class);
+        xstream.addPermission(AnyTypePermission.ANY);
     }
-    public void saveToXML(Object o){
-        xstream.toXML(o);
+    public void saveToXML(Settings o) throws IOException {
+        String xml = xstream.toXML(o);
+        File file = new File("src/Save.xml");
+        if(file.exists()){
+            FileWriter fw = new FileWriter("src/Save.xml");
+            fw.write(xml);
+            fw.close();
+        }
+    }
+    public Settings readFromXML() throws IOException {
+        File file = new File("src/Save.xml");
+        if(file.exists()){
+            return (Settings)xstream.fromXML(Files.readString(Paths.get("src/Save.xml")));
+        }
+        return null;
     }
 }
